e849656bc808766a28583cbc12efb3024dd79a39
Nikhila-perfect <73355819+Nikhila-perfect@users.noreply.github.com>
2020-10-24 22:29:07 +0530

    Update Birthday.java

--- Birthday.java
+++ Birthday.java
@@ -100,3 +100,41 @@ public class Birthday implements Serializable {
 		}
 		return day;
 	}
+public String calcMonth(int mm)
+	{
+		switch(mm)
+		{
+			case 1 : month="January";
+				 break;
+			case 2 : month="February";
+				 break;
+			case 3 : month="March";
+				 break;
+			case 4 : month="April";
+				 break;
+			case 5 : month="May";
+				 break;
+			case 6 : month="June";
+				 break;
+			case 7 : month="July";
+				 break;
+			case 8 : month="August";
+				 break;
+			case 9 : month="September";
+				 break;
+			case 10 : month="October";
+				  break;
+			case 11 : month="November";
+				  break;
+			case 12 : month="December";
+				  break;
+		}
+		return month;
+
+	}
+	public String toString()
+	{
+		return (calcDay(dd,mm,yyyy)+","+" "+calcMonth(mm)+" "+getDay(dd)+","+" "+getYear(yyyy));
+	}
+
+}
